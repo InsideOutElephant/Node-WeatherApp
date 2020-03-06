c266332c4d47dc6df620cfe050631b9095a5dac1
McNally <neil.mcnally@sap.com>
2020-03-06 11:42:50 +0000

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     locationPara.textContent = 'Loading...'
     const location = document.querySelector('input').value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 console.log(data.error)
